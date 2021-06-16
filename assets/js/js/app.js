7beebd518f0fcc25184f1eb212af7aed4380d8f1
Miro Frankovic <miro.frankovic@gmail.com>
2021-06-16 22:01:58 +0000

    Add structure to js

---
+++ assets/js/js/app.js
@@ -0,0 +1,130 @@
+/**
+   * Array with image source links to be retrieved and inserted into the HTML when cards are created
+   */
+
+const myCard =[];
+
+// Main menu section
+const mainMenuSection = document.getElementById("main-menu-section");
+
+
+// Game arena section
+
+
+
+// Game level modal
+
+
+
+// Highscores modal
+
+
+
+// Game win modal
+
+
+// Times up modal
+
+
+// Clear confirmation modal
+
+
+
+
+
+// ----------------------- Buttons
+// Back to main menu buttons
+
+
+
+
+
+// Audio buttons
+
+
+
+
+
+// ----------------------- Audio
+// Audio files
+
+
+
+
+// Volume on/off toggle
+
+
+
+
+
+// ----------------------- Game start
+
+
+
+
+
+
+// Create cards in game arena according to the selected level
+// Hide main menu section and show game arena section
+// Set and display points, level, timer, turn counter in their starting values
+// Call functions to shuffle cards, start timer and active score system
+
+
+
+
+
+// Shuffle cards before each game
+
+
+
+
+
+
+// Add figure on the other side of card
+// Check if one or two cards reversed
+// Check if two cards are the same
+// Add lock to prevent reverse more than 2 cards before check
+// Update turn counter with every two cards reversed
+// Call scoring function to add points when 2 cards match and subtract points when don't
+
+
+
+
+
+
+// When 2 reversed cards match keep them on board
+// Remove lock
+// Update pairs variable and if 0 clear time interval
+// Play finish audio when all pairs reversed
+
+
+
+
+
+// When 2 reversed cards do not match restore them
+// Remove lock
+
+
+
+
+// Game timer
+// Set interval
+// Clear interval when times up or quit button hit
+// Show times up modal when time = 0
+
+
+
+
+// Score system
+// Show game win modal when all pairs match
+
+
+
+// Play again when times up
+
+
+
+
+
+// Save score to local storage
+// Show score in highscores modal
